a56a4bf9045d8830abff2e1f3edf00fa941136b5
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-07 01:08:15 +0000

    initial member controller and attribute name changes in member view

---
+++ src/controller/member_controller.py
@@ -0,0 +1,42 @@
+from src.factories.premium_member_factory import PremiumMemberFactory
+from src.factories.regular_member_factory import RegularMemberFactory
+from src.factories.trial_member_factory import TrialMemberFactory
+from src.view.member_view import MemberView
+
+
+class MemberController:
+
+    def __init__(self):
+        self.__factories = {
+            "premium": PremiumMemberFactory,
+            "regular": RegularMemberFactory,
+            "trial": TrialMemberFactory
+        }
+        self.__members = []
+        self.__view = MemberView
+
+    def create_member(self, member_type: str, *args):
+        if member_type == "premium":
+            member_factory = self.__factories.get(member_type)
+            member_object = member_factory.create_member(*args)
+            self.__members.append(member_object)
+            return member_object
+
+        elif member_type == "regular":
+            member_factory = self.__factories.get(member_type)
+            member_object = member_factory.create_member(*args)
+            self.__members.append(member_object)
+            return member_object
+
+        elif member_type == "trial":
+            member_factory = self.__factories.get(member_type)
+            member_object = member_factory.create_member(*args)
+            self.__members.append(member_object)
+            return member_object
+        else:
+            raise ValueError("Incorrect member type!")
+
+    def read_member(self):
+        for member in self.__members:
+            view_object = self.__view(member)
+            print(view_object)
--- src/view/member_view.py
+++ src/view/member_view.py
@@ -3,11 +3,11 @@ from src.model.members.abstract_member_model import MemberModel
 
 class MemberView:
 
-    def __init__(self, member_type: MemberModel):
-        self.__member_type = member_type
+    def __init__(self, member: MemberModel):
+        self.__member = member
 
     def render(self):
-        details = self.__member_type.get_details()
+        details = self.__member.get_details()
         details_str = "\n".join(str(value) for value in details)
         print("\nMEMBER DETAILS: ")
         print(details_str) # KWARGS WOULD WORK SO MUCH BETTER FOR THIS ...
