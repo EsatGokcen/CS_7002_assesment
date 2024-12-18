6bbbebab44679eb211610022f134739e167c910a
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-18 23:43:30 +0000

    implemented assertEqual in unittest for TestManager class

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -7,7 +7,10 @@ class TestManager(unittest.TestCase):
 
     def test_create_manager(self):
         manager_controller = ManagerController()
-        manager_controller.create_manager(name="Esat")
+        manager = manager_controller.create_manager(name="Esat")
 
         # Validate that manager creation was successful
-        self.assertTrue(manager_controller.model.get_manager_name(), "Esat")
+
+        self.assertEqual(manager_controller.model, manager, "Manager is not created as expected!")
+
+        #self.assertTrue(manager_controller.model.get_manager_name(), "Esat")
