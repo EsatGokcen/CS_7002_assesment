074a23bd79254806e4cac75c9f32afb13057e885
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-18 23:14:48 +0000

    added type hints to gym controller

--- src/controller/gym_controller.py
+++ src/controller/gym_controller.py
@@ -1,6 +1,8 @@
 from src.controller.manager_controller import ManagerController
 from src.controller.workout_zone_controller import WorkoutZoneController
 from src.model.gym_model import GymModel
+from src.model.manager_model import GymManagerModel
+from src.model.workout_zone_model import WorkoutZoneModel
 from src.view.gym_view import GymView
 
 
@@ -10,23 +12,24 @@ class GymController:
         self.model = GymModel(city="London", manager=None)
         self.__view = GymView(self.model)
 
-    def create_manager(self, name: str):
+    def create_manager(self, name: str) -> GymManagerModel:
         manager_controller = ManagerController()
         manager = manager_controller.create_manager(name)
         self.model.set_gym_manager(manager)
         return manager
 
-    def create_workout_zone(self, type: str, capacity: int): # NEED TO BE ABLE TO ADD EQUIPMENT TO WORKOUT ZONES
+    # NEED TO BE ABLE TO ADD EQUIPMENT TO WORKOUT ZONES
+    def create_workout_zone(self, type: str, capacity: int) -> WorkoutZoneModel:
         workout_zone_controller = WorkoutZoneController()
         workout_zone = workout_zone_controller.create_workout_zone(type, capacity)
         self.model.add_workout_zone(workout_zone)
         return workout_zone
 
-    def update_city(self, city: str):
+    def update_city(self, city: str) -> str:
         self.model.set_gym_city(city)
         return self.model.get_gym_city()
 
-    def update_manager(self, name: str):
+    def update_manager(self, name: str) -> GymManagerModel:
         manager_controller = ManagerController()
         manager = manager_controller.create_manager(name)
         self.model.set_gym_manager(manager)
