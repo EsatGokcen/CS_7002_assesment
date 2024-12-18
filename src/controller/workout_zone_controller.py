aa81d432d8db1a4348325cd654f7bf4a63c25a4e
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-18 23:17:29 +0000

    added type hints to workout zone controller

--- src/controller/workout_zone_controller.py
+++ src/controller/workout_zone_controller.py
@@ -1,3 +1,4 @@
+from src.model.equipment_model import EquipmentModel
 from src.model.workout_zone_model import WorkoutZoneModel
 from src.view.workout_zone_view import WorkoutZoneView
 from src.controller.equipment_controller import EquipmentController
@@ -9,14 +10,15 @@ class WorkoutZoneController:
         self.model = WorkoutZoneModel("Yoga Zone", 15)
         self.__view = WorkoutZoneView(self.model)
 
-    def create_workout_zone(self, type: str, capacity: int): # REMOVE FROM gym_location_controller
+    def create_workout_zone(self, type: str, capacity: int) -> WorkoutZoneModel:
         self.model.set_type(type)
         self.model.set_capacity(capacity)
         return self.model
 
-    def create_equipment(self, connection: EquipmentController, name: str, type: str):
+    def create_equipment(self, connection: EquipmentController, name: str, type: str) -> list[EquipmentModel]:
         equipment = connection.create_equipment(name, type)
         self.model.set_equipment(equipment)
+        return self.model.get_list_of_equipments()
 
     def read_workout_zone(self):
         print(self.__view.render())
