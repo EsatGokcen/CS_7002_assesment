7d4f32c24d054a51abe2adf5ccde23511f5b32ab
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-18 23:48:03 +0000

    implemeneted assertEqual in unittest for TestWorkoutZone class and started a new method

--- tests/test_workout_zone.py
+++ tests/test_workout_zone.py
@@ -7,8 +7,15 @@ class TestWorkoutZone(unittest.TestCase):
 
     def test_create_workout_zone(self):
         workout_zone_controller = WorkoutZoneController()
-        workout_zone_controller.create_workout_zone(type="cardio", capacity=20)
+        workout_zone = workout_zone_controller.create_workout_zone(type="cardio", capacity=20)
 
         # Validate that workout zone creation was successful
-        self.assertTrue(workout_zone_controller.model.get_workout_zone_type(), "cardio")
-        self.assertTrue(workout_zone_controller.model.get_workout_zone_capacity(), 20)
+
+        self.assertEqual(workout_zone_controller.model, workout_zone, "Workout zone is not created as expected!")
+
+        # these were implemented wrong as they do not check for "cardio" and 20 specifically!
+        #self.assertTrue(workout_zone_controller.model.get_workout_zone_type(), "cardio")
+        #self.assertTrue(workout_zone_controller.model.get_workout_zone_capacity(), 20)
+
+    def test_create_equipment(self):
+        pass
