feba0035c913e7a509313e9760fb55c698f6cf73
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-19 15:43:37 +0000

    updated create workout zone method in gym controller to be able to add a list of equipments to workout zone

--- src/controller/gym_controller.py
+++ src/controller/gym_controller.py
@@ -1,5 +1,6 @@
 from src.controller.manager_controller import ManagerController
 from src.controller.workout_zone_controller import WorkoutZoneController
+from src.model.equipment_model import EquipmentModel
 from src.model.gym_model import GymModel
 from src.model.manager_model import GymManagerModel
 from src.model.workout_zone_model import WorkoutZoneModel
@@ -19,10 +20,17 @@ class GymController:
         return manager
 
     # NEED TO BE ABLE TO ADD EQUIPMENT TO WORKOUT ZONES
-    def create_workout_zone(self, type: str, capacity: int) -> WorkoutZoneModel:
+    def create_workout_zone(self, type: str, capacity: int, list_of_equipments: list[EquipmentModel]) -> WorkoutZoneModel:
+
+        # CREATE WORKOUT ZONE
         workout_zone_controller = WorkoutZoneController()
         workout_zone = workout_zone_controller.create_workout_zone(type, capacity)
         self.model.add_workout_zone(workout_zone)
+
+        # ADD LIST OF EQUIPMENTS TO WORKOUT ZONE
+        for equipment in list_of_equipments:
+            workout_zone.set_equipment(equipment)
+
         return workout_zone
 
     def update_city(self, city: str) -> str:
