2d193a43c14ca23b2cc6385ab577a32da7ed8ac5
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-19 15:27:28 +0000

    fixed the GymView render method so it returns strings of objects in lists

--- src/view/gym_view.py
+++ src/view/gym_view.py
@@ -11,16 +11,22 @@ class GymView:
         city = self.__model.get_gym_city()
         manager = self.__model.get_gym_manager()
         workout_zones = self.__model.get_gym_workout_zones()
-        staff = self.__model.get_list_of_staff()
-        members = self.__model.get_list_of_members()
-        classes = self.__model.get_list_of_classes()
+        staff_list = self.__model.get_list_of_staff()
+        members_list = self.__model.get_list_of_members()
+        classes_list = self.__model.get_list_of_classes()
+
+        # Gets each workout zone in the list as a string
+        zones = ", ".join(str(zone) for zone in workout_zones)
+        staffs = ", ".join(str(staff) for staff in staff_list)
+        members = ", ".join(str(member) for member in members_list)
+        classes = ", ".join(str(clas) for clas in classes_list)
 
         return (
             f"\nGYM ID: {id}"
             f"\nGYM City: {city}"
             f"\nGYM Manager: {manager}"
-            f"\nGYM workout zones: {workout_zones}" # returns location instead of items , should list equipments as well
-            f"\nGYM staff: {staff}"
+            f"\nGYM workout zones: {zones}" 
+            f"\nGYM staff: {staffs}"
             f"\nGYM members: {members}"
             f"\nGYM classes: {classes}"
         )
