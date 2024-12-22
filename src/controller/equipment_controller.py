87e1799db7fa980a3af69e787438c80407bdad09
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-22 17:02:21 +0000

    changing equipment controller self.model back to how it was

--- src/controller/equipment_controller.py
+++ src/controller/equipment_controller.py
@@ -5,12 +5,13 @@ from src.view.equipment_view import EquipmentView
 class EquipmentController:
 
     def __init__(self):
-        self.model = EquipmentModel
+        self.model = EquipmentModel(name="palceholder", type="placeholder")
         self.__view = EquipmentView(self.model)
 
     def create_equipment(self, name: str, type: str) -> EquipmentModel:
-        equipment = self.model(name=name, type=type)
-        return equipment
+        self.model.set_equipment_name(name)
+        self.model.set_equipment_type(type)
+        return self.model
 
     def read_equipment(self):
         print(self.__view.render())
