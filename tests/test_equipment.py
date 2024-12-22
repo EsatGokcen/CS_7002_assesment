a274dc7c0c9d7f9ba17bcafc7bb028d820a3a2be
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-22 17:08:15 +0000

    re-made test equipment to fix test error, error fixed

--- tests/test_equipment.py
+++ tests/test_equipment.py
@@ -5,12 +5,13 @@ from src.controller.equipment_controller import EquipmentController
 class TestEquipment(unittest.TestCase):
 
     def test_create_equipment(self):
-        equipment_controller = EquipmentController()
-        equipment = equipment_controller.create_equipment(name="Bench Press", type="Barbell Weights")
+        equipment1 = EquipmentController()
+        equipment1.create_equipment(name="Bench Press", type="Barbell Weights")
 
         # Validate that equipment creation was successful
 
-        self.assertEqual(equipment_controller.model, equipment, "Equipment is not created as expected!")
+        self.assertEqual(equipment1.model.get_equipment_name(), "Bench Press", "Equipment is not created as expected!")
+        self.assertEqual(equipment1.model.get_equipment_type(), "Barbell Weights", "Equipment is not created as expected!")
 
         # These are unnecessary here as they only check if method returns True
         #self.assertTrue(equipment_controller.model.get_equipment_name(), "Bench Press")
