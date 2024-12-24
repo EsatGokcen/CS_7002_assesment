452b0c0edc102719c295b6d6a2074f68621bf7d5
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-24 15:27:56 +0000

    initial abstract member model

---
+++ src/model/member_model.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+class Member(ABC):
+
+    def __init__(self, name: str, email: str, phone_number: str):
+        self.__name = name
+        self.__email = email
+        self.__phone_number = phone_number
+        self.__username = "placeholder"
+        self.__password = "placeholder"
+        self.__health_info = "Healthy"
+
+    @abstractmethod
+    def get_member_type(self) -> str:
+        pass
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_email(self) -> str:
+        return self.__email
+
+    def get_phone_number(self) -> str:
+        return self.__phone_number
+
+    def get_username(self) -> str:
+        return self.__username
+
+    def get_password(self) -> str:
+        return self.__password
+
+    def get_health_info(self) -> str:
+        return self.__health_info
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def set_email(self, email: str):
+        self.__email = email
+
+    def set_phone_number(self, phone_number: str):
+        self.__phone_number = phone_number
+
+    def set_username(self, username: str):
+        self.__username = username
+
+    def set_password(self, password: str):
+        self.__password = password
+
+    def set_health_info(self, health_info: str):
+        self.__health_info = health_info
+
