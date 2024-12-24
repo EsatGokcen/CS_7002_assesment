2df0f00552d4ec568bce3224fba2d41611d70f22
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-24 15:41:27 +0000

    added two new attributes to staff model with getter and setters, started on staff view

--- src/model/staff_model.py
+++ src/model/staff_model.py
@@ -2,16 +2,30 @@ class StaffModel:
 
     initial_staff_id = 12345
 
-    def __init__(self, job_title: str):
+    def __init__(self, name: str, email: str,  job_title: str):
         self.__staff_id = StaffModel.initial_staff_id
         StaffModel.initial_staff_id += 1
+        self.__name = name
+        self.__email = email
         self.__job_title = job_title
 
     def get_staff_id(self) -> int:
         return self.__staff_id
 
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_email(self) -> str:
+        return self.__email
+
     def get_job_title(self) -> str:
         return self.__job_title
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def set_email(self, email: str):
+        self.__email = email
 
     def set_job_title(self, job_title: str):
         self.__job_title = job_title
---
+++ src/view/staff_view.py
@@ -0,0 +1,12 @@
+from src.model.staff_model import StaffModel
+
+
+class StaffView:
+
+    def __init__(self, staff_model: StaffModel):
+        self.__model = staff_model
+
+    def render(self):
+        return (
+            f""
+        )
