1bf4d29084db729db1148a334300c428f4ee605f
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-27 22:26:05 +0000

    initial premium member model class and new fee attribute with get, set methods for regular member model

--- src/model/premium_member_model.py
+++ src/model/premium_member_model.py
@@ -1,12 +1,12 @@
 from src.model.member_model import MemberModel
 
 
-class RegularMemberModel(MemberModel):
+class PremiumMemberModel(MemberModel):
 
     def __init__(self, name: str, email: str, phone_number: str):
         super().__init__(name=name, email=email, phone_number=phone_number)
-        self.__member_type = "regular"
-        self.__fee = 30.00 # £ per month
+        self.__member_type = "premium"
+        self.__fee = 50.00 # £ per month
 
     def get_member_type(self) -> str:
         return self.__member_type
