2121cac538f589360ff6aa125cf8021728017f31
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-28 22:28:29 +0000

    initial classes controller class with create, read and add attendee methods

---
+++ src/controller/classes_controller.py
@@ -0,0 +1,28 @@
+from src.model.classes_model import ClassesModel
+from src.model.member_model import MemberModel
+from src.model.staff_model import StaffModel
+from src.model.workout_zone_model import WorkoutZoneModel
+from src.view.classes_view import ClassesView
+from typing import Type
+
+
+class ClassesController:
+
+    def __init__(self):
+        self.model = ClassesModel(name="placeholder", date="00/00/00")
+        self.__view = ClassesView(self.model)
+
+    def create_class(self, name: str, date: str, capacity: int, teacher: StaffModel, location: WorkoutZoneModel) -> ClassesModel:
+        self.model.set_name(name)
+        self.model.set_date(date)
+        self.model.set_capacity(capacity)
+        self.model.set_teacher(teacher)
+        self.model.set_location(location)
+        return self.model
+
+    def add_attendee(self, attendee: Type[MemberModel]) -> Type[MemberModel]:
+        self.model.add_attendee(attendee)
+        return attendee
+
+    def read_class(self):
+        print(self.__view.render())
