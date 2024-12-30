79ae7f9a4f6a13ebb3b2aaba92c2baa9842486d6
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-30 13:20:13 +0000

    new attributes and methods to manager model for logging in

--- src/model/manager_model.py
+++ src/model/manager_model.py
@@ -7,6 +7,8 @@ class GymManagerModel:
         self.__manager_id = GymManagerModel.initial_manager_id
         GymManagerModel.initial_manager_id += 1 # creates a unique id for each manager
         self.__name = name
+        self.__username = "admin"
+        self.__password = "admin"
 
     def __str__(self):
         return f"Name: {self.__name}, ID: {self.__manager_id}"
@@ -17,5 +19,18 @@ class GymManagerModel:
     def get_manager_name(self) -> str:
         return self.__name
 
+    def get_username(self) -> str:
+        return self.__username
+
+    def get_password(self) -> str:
+        return self.__password
+
     def set_manager_name(self, name: str):
         self.__name = name
+
+    def set_username(self, username: str):
+        self.__username = username
+
+    def set_password(self, password: str):
+        self.__password = password
+
