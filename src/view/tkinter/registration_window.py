a9fe521c5ec64fb85964755efe3c2f5151c06437
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-30 14:19:25 +0000

    added new labels to registration window to help with member creation

--- src/view/tkinter/registration_window.py
+++ src/view/tkinter/registration_window.py
@@ -11,6 +11,21 @@ class RegistrationWindow:
         self.master.geometry("600x400")
         #self.master.configure(bg="#b0c4de") # For colour
 
+        # Name Label and Entry
+        tk.Label(master, text="Name: ").pack(pady=5)
+        self.name_entry = tk.Entry(master)
+        self.name_entry.pack(pady=5)
+
+        # Email Label and Entry
+        tk.Label(master, text="Email: ").pack(pady=5)
+        self.email_entry = tk.Entry(master)
+        self.email_entry.pack(pady=5)
+
+        # Phone Number Label and Entry
+        tk.Label(master, text="Phone Number: ").pack(pady=5)
+        self.phone_number_entry = tk.Entry(master)
+        self.phone_number_entry.pack(pady=5)
+
         # Username Label and Entry
         tk.Label(master, text="Username:").pack(pady=5)
         self.username_entry = tk.Entry(master)
@@ -33,11 +48,14 @@ class RegistrationWindow:
         tk.Button(master, text="Back", command=self.go_back).pack(pady=5)
 
     def register(self):
+        name = self.name_entry.get()
+        email = self.email_entry.get()
+        phone_number = self.phone_number_entry.get()
         username = self.username_entry.get()
         password = self.password_entry.get()
         confirm_password = self.confirm_password_entry.get()
 
-        if not username or not password or not confirm_password:
+        if not name or not email or not phone_number or not username or not password or not confirm_password:
             messagebox.showerror("Registration Failed", "All fields are required")
             return
 
