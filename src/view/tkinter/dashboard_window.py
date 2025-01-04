f902f2cf49870e47189decd20b7d6ed21d1596dc
Esat Gokcen <esatgokcen173@gmail.com>
2025-01-04 22:54:56 +0000

    blueprint of methods, navigation section and more labels for dashboard window

--- src/view/tkinter/dashboard_window.py
+++ src/view/tkinter/dashboard_window.py
@@ -14,8 +14,29 @@ class DashboardWindow:
         # Show Member Details
         tk.Label(master, text="Member Details:", font=("Arial", 24)).pack(pady=10)
         tk.Label(master, text=f"Name: {self.get_name()}").pack(pady=5)
+        tk.Label(master, text=f"Membership status: {self.get_membership_status()}").pack(pady=5)
+        tk.Label(master, text=f"Booked classes: {self.get_booked_classes()}").pack(pady=5)
+
+        # Book a class
+
+        # Navigation section
+        tk.Label(master, text="Navigation Section:", font=("Arial", 18)).pack(pady=15)
+        # Edit Member Details section
+        # Payment information section
+        # Back Button
+        tk.Button(master, text="Back", command=self.go_back).pack(pady=5)
 
     def get_name(self):
         pass
+
+    def get_membership_status(self):
+        pass
+
+    def get_booked_classes(self):
+        pass
+
+    def go_back(self):
+        self.controller.show_login_window()
+
 
 
