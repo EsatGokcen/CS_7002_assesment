3208d396cae562d06dca8786794455ed4823e5d4
Esat Gokcen <esatgokcen173@gmail.com>
2025-01-06 17:17:38 +0000

    completed payment information window

--- src/view/tkinter/payment_information_window.py
+++ src/view/tkinter/payment_information_window.py
@@ -11,10 +11,27 @@ class PaymentInformationWindow:
         self.master.geometry("600x600")
 
         # Show Payment Information
-        tk.Label(master, text="Payment Information:", font=("Arial", 22)).pack(pady=10)
+        tk.Label(master, text="Payment Information:", font=("Arial", 20)).pack(pady=10)
+        tk.Label(master, text=f"Subscription type: {self.member.get_member_type()}").pack(pady=5)
+        tk.Label(master, text=f"Amount due: {self.member.get_payment_amount()}").pack(pady=5)
+        tk.Label(master, text=f"Subscription duration: {self.member.get_payment_duration()}").pack(pady=5)
 
         # Edit Payment Information
-        tk.Label(master, text="Edit Payment Information:", font=("Arial", 22)).pack(pady=10)
+        tk.Label(master, text="Edit Payment Information:", font=("Arial", 20)).pack(pady=10)
+        # Subscription Type Label and Dropdown
+        tk.Label(master, text="Select Subscription Type:").pack(pady=5)
+        self.subscription_var = tk.StringVar(master)
+        self.subscription_var.set("Select a type")
+        self.subscriptions = ["Regular", "Premium"]
+        self.subscription_dropdown = tk.OptionMenu(master, self.subscription_var, *self.subscriptions)
+        self.subscription_dropdown.pack(pady=5)
+        # Payment Frequency Label and Dropdown
+        tk.Label(master, text="Select Payment Frequency:").pack(pady=5)
+        self.payment_var = tk.StringVar(master)
+        self.payment_var.set("Select a frequency")
+        self.payment_frequencies = ["Monthly", "Quarterly", "Annually"]
+        self.payment_dropdown = tk.OptionMenu(master, self.payment_var, *self.payment_frequencies)
+        self.payment_dropdown.pack(pady=5)
 
         # Submit Button
         tk.Button(master, text="Submit", command=self.submit).pack(pady=15)
@@ -22,8 +39,36 @@ class PaymentInformationWindow:
         tk.Button(master, text="Back", command=self.go_back).pack(pady=5)
 
     def submit(self):
-        # Logic
-        return messagebox.showinfo("Submit", "New payment information submitted successfully!")
+        amount = "£0"
+        duration = "0 days"
+
+        selected_subscription = self.subscription_var.get()
+        selected_payment = self.payment_var.get()
+
+        if selected_subscription == "Regular":
+            if selected_payment == "Monthly":
+                amount = "£30"
+                duration = "1 month"
+            elif selected_payment == "Quarterly":
+                amount = "£90"
+                duration = "3 months"
+            elif selected_payment == "Annually":
+                amount = "£330"
+                duration = "12 months"
+        elif selected_subscription == "Premium":
+            if selected_payment == "Monthly":
+                amount = "£50"
+                duration = "1 month"
+            elif selected_payment == "Quarterly":
+                amount = "£150"
+                duration = "3 months"
+            elif selected_payment == "Annually":
+                amount = "£550"
+                duration = "12 months"
+        return (
+            messagebox.showinfo("Submit", "New payment information submitted successfully!"),
+            messagebox.showinfo("Details", f"New amount due: {amount}, subscription duration: {duration}, subscription type: {selected_subscription}")
+        )
 
     def go_back(self):
         self.controller.show_dashboard_window()
